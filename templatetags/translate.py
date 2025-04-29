3856a4e8d22ad4bc040aaddf82d3f9cdbbe6532b
Bonfim Justino <bonfim1999@gmail.com>
2025-04-29 17:44:56 -0300

    feat: adiciona imagens no componente de tradução

--- templatetags/translate.py
+++ templatetags/translate.py
@@ -1,10 +1,12 @@
 from django import template
+from django.templatetags.static import static
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 
 @register.inclusion_tag('translate.html')
-def google_translate_link(language, default="pt-BR", class_style=None, label="", data_lang=""):
+def google_translate_link(language, default="pt-BR", class_style=None, label="", image="", data_lang="", alt=""):
     return {
         "language": default,
         "type": "vertical",
@@ -12,5 +14,7 @@ def google_translate_link(language, default="pt-BR", class_style=None, label="",
         "class": class_style,
         "target_language": language,
         "label": label,
-        "data_lang": data_lang
+        "image": f'img/{image}',
+        "data_lang": data_lang,
+        "alt": alt
     }
