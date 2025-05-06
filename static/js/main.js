014cf088953e0342b8ea64ff870922dfb84fac80
Mailson Queiroz <promailson@gmail.com>
2025-05-06 12:04:51 -0300

    feat: calendário adicionada na página inicial

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var calendarEl = document.getElementById("calendar");
-  var calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: "dayGridMonth",
-    locale: "pt-br",
-  });
-  calendar.render();
-});
-
-document.addEventListener("DOMContentLoaded", function () {
   const languageOptions = document.querySelectorAll(".language-option");
   const selectedLanguageSpan = document.getElementById("selectedLanguage");
 
@@ -32,6 +23,97 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+var calendarEl = document.getElementById("calendar");
+var calendar = new FullCalendar.Calendar(calendarEl, {
+  timeZone: "America/Sao_Paulo",
+  initialView: "dayGridMonth",
+  locale: "pt-br",
+  themeSystem: "standard",
+  headerToolbar: {
+    left: "prev",
+    center: "title",
+    right: "next",
+  },
+  events: [
+    {
+      title: "Reunião de Equipe",
+      start: "2025-05-01",
+      allDay: true,
+      extendedProps: {
+        description: "Discussão sobre o projeto X.",
+      },
+    },
+    {
+      title: "Entrega do Relatório",
+      start: "2025-05-01",
+      allDay: true,
+      extendedProps: {
+        description: "Prazo final para envio do relatório mensal.",
+        link: "https://example.com/relatorio",
+      },
+    },
+    {
+      title: "Aniversário do João",
+      start: "2025-05-02",
+      allDay: true,
+      extendedProps: {
+        description: "Comemoração do aniversário do João.",
+        link: "https://example.com/joao-aniversario",
+      },
+    },
+    {
+      title: "Aniversário do João",
+      start: "2025-05-30",
+      allDay: true,
+      extendedProps: {
+        description: "Comemoração do aniversário do João.",
+        link: "https://example.com/joao-aniversario",
+      },
+    },
+  ],
+  eventDisplay: "background",
+  dayMaxEventRows: 10,
+  eventDidMount: function (info) {
+    const allEvents = info.view.calendar.getEvents();
+    const targetDate = info.event.startStr;
+    const sameDayEvents = allEvents.filter(
+      (event) => event.startStr === targetDate
+    );
+
+    const tooltipContent = `
+      <div style="min-width: 200px; padding: 10px">
+        ${sameDayEvents
+          .map(
+            (e) =>
+              `<div style="margin-bottom:4px;">
+            <strong>${e.title}</strong><br>
+            ${e.extendedProps.description || "Sem descrição."}<br>
+            ${
+              e.extendedProps.link
+                ? `<a href="${e.extendedProps.link}" target="_blank">Ver mais</a>`
+                : ""
+            }
+          </div>`
+          )
+          .join(
+            '<div style="border-top:1px solid #ccc; margin:10px 0;"></div>'
+          )}
+      </div>
+    `;
+
+    tippy(info.el, {
+      content: tooltipContent,
+      allowHTML: true,
+      interactive: true,
+      placement: "bottom",
+      theme: "light",
+      animation: "scale-subtle",
+      delay: 0,
+    });
+  },
+});
+calendar.render();
 
 const tooltipTriggerList = document.querySelectorAll(
   '[data-bs-toggle="tooltip"]'
